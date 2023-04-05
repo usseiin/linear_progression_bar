02baaca5b4177573b33830c29be9bdfcb7e58e80
usseiin <sirkenny.hk@gmail.com>
2023-04-05 17:07:12 +0100

    fix bug

--- lib/linear_progression_bar.dart
+++ lib/linear_progression_bar.dart
@@ -1,7 +1,3 @@
 library linear_progression_bar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/linear_progression_bar.dart';
